9b0f1437c15607a1d57eb749b1620fa0b6df1593
Shirong Guo <shannongsr@yeah.net>
2022-11-29 15:23:50 +0800

    Add files via upload

---
+++ mac.py
@@ -0,0 +1,9 @@
+# coding=utf-8
+import uuid
+import os
+import sys
+mac = uuid.UUID(  int=uuid.getnode() ).hex[-12:]
+while(mac == '0242alafd239'):
+    os.system("./video_camera1_dehaze")
+print(mac)
+sys.exit()
